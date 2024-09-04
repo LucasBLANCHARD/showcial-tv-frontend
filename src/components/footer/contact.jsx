c57b2df429d6bdb98ab335d94dfa0589e908f00b
Lucas Blanchard <lucas.blanchard2021@campus-eni.fr>
2024-09-04 14:48:59 +0200

    linkedin in contact

--- src/components/footer/contact.jsx
+++ src/components/footer/contact.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './contact.scss'; // Importer les styles spécifiques si nécessaire
+import './contact.scss';
 import { t } from 'i18next';
 
 const Contact = () => {
@@ -8,10 +8,15 @@ const Contact = () => {
       <h1>{t('footer.contact')}</h1>
       <p>{t('footer.any-question')}.</p>
       <p>
-        <strong>{t('footer.email')}:</strong> {t('footer.my-email')}
+        <strong>{t('footer.email')}: </strong> {t('footer.my-email')}
+      </p>
+      <p>
+        <strong>{t('footer.linkedin')}: </strong>
+        <a href="https://www.linkedin.com/in/lucas-blchd" target="_blank" rel="noopener noreferrer">
+          {t('footer.my-linkedin')}
+        </a>
       </p>
     </div>
   );
 };
-
 export default Contact;
